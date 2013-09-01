3c4a448d7c9667c3739ee49a4a7a968208e53670
doijads <sachinatcybage@gmail.com>
2013-09-02 00:08:36 +0530

    conflict resolvd

--- application/models/DbTable/Usersdetails.php
+++ application/models/DbTable/Usersdetails.php
@@ -6,8 +6,3 @@ class Application_Model_DbTable_UsersDetails extends Zend_Db_Table_Abstract
     protected $_name    = 'user_details';
 }
 ?>
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 11dbc85715960d0a16f57d59a3db15f5d571b6fa
