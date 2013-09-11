216f7007ffd3df9871a792007d0576ba497e2466
doijads <sachinatcybage@gmail.com>
2013-09-12 00:07:33 +0530

    done with email functionality

--- library/App/email.php
+++ library/App/email.php
@@ -22,12 +22,12 @@ class App_Email
 //        exit;
         $isSend = false;
         if( !empty($recipient) ){
-//            if( mail($recipient, $subject, $message,     $fromName) ){
-//                $isSend = true;                             
-//            }else{
-//                $isSend = false;
-//            }            
-//            
+            if( mail($recipient, $subject, $message,     $fromName) ){
+                $isSend = true;                             
+            }else{
+                $isSend = false;
+            }            
+            
      
             
             return $isSend; 
@@ -35,7 +35,7 @@ class App_Email
         
     }
     public static function sendEmailToLawyer($recipient){
-        $subject = 'Welcome Subject';
+        $subject = 'Welcome User';
         $headers = "From: LCM>\r\n";
         $headers .= "MIME-Version: 1.0\n";
         $headers .= "Content-Type: text/html; charset=utf-8\n";
@@ -44,13 +44,16 @@ class App_Email
         $fromEmail = "admin@lcm.com";
         $fromName  = "Admin";
                 
-        $message = "";
+        $message = "Welcome..";
         $message .= "<html xmlns='http://www.w3.org/1999/xhtml'><head><meta http-equiv='Content-Type content='text/html; charset=utf-8' /></head><body style=\"font-family: 'Droid Sans', sans-serif;\">";
-        $message .= "<p>You have been registered with our system.</p>";
+        $message .= "<p>You have been registered with our system.</p><n/>";
+        $message .= "<p>You can login to system with following credentails.</p><n/>";
+        $message .= "<p><b>UserName : </b>".$recipient['email']."</p><n/>";
+        $message .= "<p><b>Password : </b>".$recipient['password']."</p><n/>";
         $message .= "</body></html>";         
         $message = rtrim($message);
                 
-        self::send($recipient,$subject, $message, $headers, $fromEmail, $fromName);        
+        self::send($recipient['email'],$subject, $message, $headers, $fromEmail, $fromName);        
     }
     
     public static function sendEmailToClient($recipient){
@@ -62,93 +65,5 @@ class App_Email
         
         self::send($recipient,$subject, $message, $headers, $fromEmail, $fromName);
     }
-    
-            
-    
-//    public static function getRole()
-//    {
-//        $auth = Zend_Auth::getInstance();
-//        $role = 'guest';
-//        
-//        if ($auth->hasIdentity()) {
-//
-//            $user = self::getLoggedUser();
-//           
-//            //some time we get the
-//            //email address as identity.
-//            $userRoles = array();
-//            if ( is_a( $user, 'Model_User' ) ) {
-//                $userRoles = $user->roleTypeIds;
-//            }
-//
-//            if ( in_array(6, $userRoles) ) {
-//                $role = 'super-admin';
-//            } else if ( in_array(5, $userRoles) ) {
-//                $role = 'admin';
-//            } else if ( (in_array(1, $userRoles) && in_array(2, $userRoles)) || 
-//                        (in_array(1, $userRoles) && in_array(4, $userRoles)) ||
-//                        (in_array(2, $userRoles) && in_array(3, $userRoles)) ||
-//                        (in_array(3, $userRoles) && in_array(4, $userRoles)) ) {
-//                $role = 'publisher_advertiser';
-//            } else if (in_array(1, $userRoles)||in_array(3, $userRoles)) {
-//                //publisher or publisher manager
-//                $role = 'publisher';
-//            } else if (in_array(2, $userRoles)||in_array(4, $userRoles)) {
-//                //advertiser or advertiser manager
-//                $role = 'advertiser';
-//            }
-//            
-//            /*
-//              if($user->roleTypeId == 1 || $user->roleTypeId == 2) {
-//              return 'publisher';
-//              } else if ($user->roleTypeId == 3) {
-//              return 'advertiser';
-//              } else if($user->roleTypeId == 4) {
-//              return 'publisher_advertiser';
-//              } else if($user->roleTypeId == 5) {
-//              return 'admin';
-//              } else {
-//              return 'guest';
-//              }
-//             */
-//        }
-//        
-//        return $role;
-//    }
-//     
-//    public static function isLogged()
-//    {
-//        $auth = Zend_Auth::getInstance();
-//        return $auth->hasIdentity();
-//    }
-//
-//    public static function getUserId() 
-//    {
-//        $userId = null;
-//        $auth = Zend_Auth::getInstance();
-//        if (is_a($auth, 'Zend_Auth')) {
-//            $user = $auth->getIdentity();
-//            if (is_a($user, 'Model_User')) {
-//                $userId = $user->id;
-//            }
-//        }
-//        return $userId;
-//    }
-//    
-//    /**
-//     * @return Model_User
-//     */
-//    public static function getLoggedUser()
-//    {
-//        $auth = Zend_Auth::getInstance();
-//        return $auth->getIdentity();
-//    }
-//	
-//    /**
-//     * @return bool
-//     */
-//    public static function isAdmin() {
-//        return in_array(self::getRole(), array('admin', 'super-admin'));
-//    }
-//  
+                 
 }
